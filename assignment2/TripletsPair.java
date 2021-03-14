5f4a39da5f2f3c848f869c24f02367190f9350f0
Shams66789 <rajansari66789@gmail.com>
2021-03-14 20:00:32 +0530

    Question 15 of Assignment 2

---
+++ assignment2/TripletsPair.java
@@ -0,0 +1,26 @@
+package CFCJavaAssignment.assignment2;
+
+import java.util.Scanner;
+
+public class TripletsPair {
+    public static void main(String[] args) {
+        int[] array = {3, 1, 2, 9, 7, 5, -1, 6};
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the target below: ");
+        int target = s.nextInt();
+
+        findtriplets(array, target);
+    }
+
+    public static void findtriplets(int[] arr,int k ) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int l = j + 1; l < arr.length; l++) {
+                    if (arr[i] + arr[j] + arr[l] == k){
+                        System.out.print(" (" + arr[i] + "," + arr[j] + "," + arr[l] + ") ");
+                    }
+                }
+            }
+        }
+    }
+}
